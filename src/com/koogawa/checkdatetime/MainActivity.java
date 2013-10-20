cee1c7adc957e8e7ebb8fdc539e05c7bd4a80804
koogawa <koogawa.app@gmail.com>
2013-10-20 19:53:33 +0900

    Change variables.

--- src/com/koogawa/checkdatetime/MainActivity.java
+++ src/com/koogawa/checkdatetime/MainActivity.java
@@ -1,17 +1,45 @@
 package com.koogawa.checkdatetime;
 
-import android.os.Bundle;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private TextView myDateTimeView;
+	private Button myShowDate;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		myDateTimeView = (TextView) findViewById(R.id.currentTimeView);
+		myShowDate = (Button) findViewById(R.id.button);
+		
+		myShowDate.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				showCurrentDate();
+			}
+		});
+		showCurrentDate();
 	}
 
+	private void showCurrentDate() {
+		SimpleDateFormat sdf =
+				new SimpleDateFormat("yyyy.MM.dd '/' hh:mm:ss a zzz");
+		Date currentTime = new Date();
+		myDateTimeView.setText(sdf.format(currentTime));
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
